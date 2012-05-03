aa1bbc18fa8917c61e7d7ee286c89da7f9904a74
deepak <dpak.cherian@gmail.com>
2012-05-02 22:41:45 -0400

    Signed-off-by: deepak <dpak.cherian@gmail.com>

--- analyze_edge_2d.m
+++ analyze_edge_2d.m
@@ -10,19 +10,24 @@ clear all
 
 dir = 'E:\Work\instability\ROMS\si_part\edge\2D\';
 %dirs = {'run01','run02','run03','run04','run05','run06','run07'};
-%dirs = {'run02'};
-dirs = {'run01','run02','run03','run05','run06','run08'}; % 4 and 7 are outliers (PVmin / PVmid > 1.1 - greater wavelength too)
-runx = [01 02 03 05 06 08];
+%dirs = {'run08'};
+dirs = {'run01','run02','run03','run05','run06','run08','run09'}; % 4 and 7 are outliers (PVmin / PVmid > 1.1 - greater wavelength too)
+runx = [01 02 03 05 06 08 09];
 fname = 'ocean_his.nc';
+volume = {};
+
+ennames = {'energy-left.mat','energy-right.mat'};
 
 plot_flag = 0;
 redo_en = 0;
 redo_pv = 0;
 
+thresh = 0.5; % threshold for energy
+
 for ii=1:length(dirs)
     cd([dir dirs{ii}]);
     
-    % read data    
+    % get grid details    
     roms_grid = roms_get_grid(fname,fname,0,1);
     misc = roms_load_misc(fname);
     time = ncread(fname,'ocean_time');
@@ -32,119 +37,130 @@ for ii=1:length(dirs)
     dx = mean(mean(diff(roms_grid.x_rho,1,2)));
     dy = mean(mean(diff(roms_grid.y_rho,1,1)));
     dz = squeeze(diff(roms_grid.z_r(:,1,1)));
+    
+    xu = roms_grid.x_u(1,:);
+    nx = length(xu);
+    range = {[1:floor(nx/2)], [floor(nx/2)+1:nx]};
 
     xmid = ceil(size(roms_grid.x_rho,2)/2);
     ymid = ceil(size(roms_grid.y_rho,1)/2);
     zmid = ceil(size(roms_grid.z_r  ,1)/2);
     
-    j = ymid;
+       j = ymid;
     yind = ymid;
     
+    % read data
     temp = squeeze(double(ncread(fname,'temp',[1 1 1 1],[Inf Inf Inf Inf])));
     v = squeeze(double(ncread(fname,'v',[1 1 1 1],[Inf Inf Inf Inf])));
     u = squeeze(double(ncread(fname,'u',[1 1 1 1],[Inf Inf Inf Inf])));
     temp_mid = squeeze(double(ncread(fname,'temp',[1 ymid 1 1],[Inf 1 Inf 1])));
-
-    if ~exist('energy-avg-x.mat','file') || redo_en == 1, roms_energy(fname,[],4,1,0); end
-    if ~exist('ocean_pv.nc','file') || redo_pv == 1, roms_pv(fname,[1 1]); end
-
-    load energy-avg-x.mat
-
-    % figure out width / wavelength of edge effects region
-    % I AM SMOOTHING THE GROWTH RATE CURVE HERE
-    [peaks,locs] = findpeaks(conv(A,[1 1]/2,'valid'));
-    %tAmax = time_A(find(A == max(A)));
-    tAmax = time_A(locs(1));
-     tind = find_approx(time,tAmax,1);
-
-    % NEED TO DO BETTER AVERAGING? ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    % do this calculation at a bunch of depths and average? <-- better estimate of the width
-    um   = mean(u(:,j,:,:),1);
-    up = squeeze(bsxfun(@minus,u(:,j,:,:),um));
-    data = up(:,zmid,tind);
-    thresh = 0.5;
     
-    % Calculate gradients
-    M2 = g*misc.Tcoef*avg1(squeeze(diff(temp(:,yind,:,tind),1,1)),2)./dx;
-    N2 = g*misc.Tcoef*avg1(bsxfun(@rdivide,diff(squeeze(temp(:,yind,:,tind)),1,2),permute(dz,[3 1 2])),1);
-
-    xu = roms_grid.x_u(1,:);
-    dx = mean(diff(xu));
-    nx = length(xu);
-    range = {[1:floor(nx/2)], [floor(nx/2)+1:nx]};
-
-    [wave,period,scale,coi] = wavelet(data,dx,1);
-    enwave = abs(wave).^2;
-
-    % plot energy 'spectrum'
-    if plot_flag
-        h1 = figure;
-        pcolor(xu/1000,log(period),enwave); shading interp
-        hold on; revz
-        plot(xu/1000,log(coi),'k','LineWidth',1.5);
-        xlabel('X (km)'); ylabel('log(Wavenumber)');
-
-        % plot u field
-        mod_movie(fname,'u',[tind tind],{},'y','mid','pcolor;shading interp;liney(-50)');
-        hold on;
-        h2 = gcf;
-    end
-
-    % first left, then right
+    if ~exist('ocean_pv.nc','file') || redo_pv == 1, roms_pv(fname,[1 1]); end   
+    pv = squeeze(ncread('ocean_pv.nc','pv',[1 ymid 1 1],[Inf 1 Inf 1]));
+    xpv = ncread('ocean_pv.nc','x_pv');
+    
+    %%%%% find negative pv region
+    pvind = find(pv(:,zmid) < 0);
+     npvl = xpv(pvind(1))/1000; % negative pv left
+     npvr = xpv(pvind(end))/1000; % and right
+    pvind = find(pv(:,zmid) ==  pv(xmid,zmid));
+     cpvl = xpv(pvind(1))/1000; % constant pv left
+     cpvr = xpv(pvind(end))/1000; % and right
+    
+    %%%%%%%%%%%%%%%%%%% first left, then right
     for i=1:2
+        rr = range{i};
+        volume = {'x' rr(1) rr(end)};
+        % calculate / load energy diagnostics for current half
+        if ~exist(ennames{i},'file') || redo_en == 1
+            roms_energy(fname,[],volume,1,1,0); 
+            system(['move energy-avg-x.mat ' ennames{i}]);
+        end
+        load(ennames{i});
+        
+        %%%%%%%%%%%%%%%% find time of max growth rate
+        % I AM SMOOTHING THE GROWTH RATE CURVE HERE
+        %[peaks,locs] = findpeaks(conv(A./max(A),[1 1]/2,'valid'),'threshold',0.01);
+        [peaks,locs] = extrema(conv(A./max(A),[1 1]/2,'valid'));
+        if locs(2) < locs(1) && peaks(2)/peaks(1) >= 0.8
+            tAmax = time_A(locs(2));
+        else
+            tAmax = time_A(locs(1));
+        end
+         tind = find_approx(time,tAmax,1);
+
+        %%%%%%%%%%%%%% Wavelet stuff
+        % NEED TO DO BETTER AVERAGING? ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        % do this calculation at a bunch of depths and average????? <-- better estimate of the width
+        um   = mean(u(:,j,:,:),1);
+        up = squeeze(bsxfun(@minus,u(:,j,:,:),um));
+        data = up(:,zmid,tind);
+
+        [wave,period,scale,coi] = wavelet(data,dx,1);
+        enwave = abs(wave).^2;
+
+        % plot energy 'spectrum'
+        if plot_flag
+            h1 = figure;
+            pcolor(xu/1000,log(period),enwave); shading interp
+            hold on; revz
+            plot(xu/1000,log(coi),'k','LineWidth',1.5);
+            xlabel('X (km)'); ylabel('log(Wavenumber)');
+
+            % plot u field
+            mod_movie(fname,'u',[tind tind],{},'y','mid','pcolor;shading interp;liney(-50)');
+            hold on;
+            h2 = gcf;
+        end
+
+        %%%%% figure out width / wavelength of edge effects region
           clear m;
-          rr = range{i};
+          
          dat = data(rr);
         xdat =   xu(rr);
 
-        % find maxima & calculate width
+        %%%%% find maxima & calculate width
            m = trapz(enwave(:,rr),1);
          ind = find(m > thresh*max(m));    
         w(i) = (ind(end)-ind(1))*dx;
         edgeloc = [(rr(1)+ind(1))*dx/1000 (rr(1)+ind(end))*dx/1000];
 
-        % calculate wavelength
+        %%%%% calculate wavelength
              m = enwave(:,rr);
         maxind = find(m == max(m(:)));
          [a,b] = ind2sub(size(m),maxind);
           l(i) = period(a);
-          
-        % find negative pv region
-         pv = squeeze(ncread('ocean_pv.nc','pv',[1 ymid 1 1],[Inf 1 Inf 1]));
-        xpv = ncread('ocean_pv.nc','x_pv');
-        
-        pvind = find(pv(:,zmid) < 0);
-         npvl = xpv(pvind(1))/1000; % negative pv left
-         npvr = xpv(pvind(end))/1000; % and right
-        pvind = find(pv(:,zmid) ==  pv(xmid,zmid));
-         cpvl = xpv(pvind(1))/1000; % constant pv left
-         cpvr = xpv(pvind(end))/1000; % and right
          
-        % find initial mean pv in the edge effects region.
-        xpvl = find_approx(xpv,edgeloc(1)*1000,1);
-        xpvr = find_approx(xpv,edgeloc(2)*1000,1);
+        %%%%% find initial mean pv in the edge effects region.
+        xpvrr = xpv(rr(1):rr(end)-1);
+        xpvl = find_approx(xpvrr,edgeloc(1)*1000,1);
+        xpvr = find_approx(xpvrr,edgeloc(2)*1000,1);
         pvedge = pv(xpvl:xpvr,:);
         meanpv = mean(pvedge(:)); max(pvedge(:)./pv(xmid,zmid));
         
         plotpv(ii,i) = meanpv;
         
-        % find mean slope in edge effects region THIS IS NOISY.
+        %%%%% Calculate gradients
+        M2 = g*misc.Tcoef*avg1(squeeze(diff(temp(rr(1):rr(end),yind,:,tind),1,1)),2)./dx;
+        N2 = g*misc.Tcoef*avg1(bsxfun(@rdivide,diff(squeeze(temp(rr(1):rr(end),yind,:,tind)),1,2),permute(dz,[3 1 2])),1);
+        
+        %%%% find mean slope in edge effects region ||||||  THIS IS NOISY.
         slope = M2(xpvl:xpvr,:)./N2(xpvl:xpvr,:);
         plotsl(ii,i) = median(slope(:));
         
-        % find Ri in edge effects region
+        %%%%% find Ri in edge effects region
          Ri = N2(xpvl:xpvr,:)./(M2(xpvl:xpvr,:)).^2 * f0^2;
         Ri0 =  median(Ri(Ri>0));
         plotri(ii,i) = Ri0;
         
-        % calculate theoretical wavelength
+        %%%%% calculate theoretical wavelength
         % first velocity scale
         xvl = find_approx(roms_grid.x_v(1,:),edgeloc(1)*1000,1);
         xvr = find_approx(roms_grid.x_v(1,:),edgeloc(2)*1000,1);
         v0 = abs(mean(v(xvl:xvr,ymid,zmid,tind)));
         l0(i) = 2*pi / (pi/sqrt(1-Ri0) * f0/v0);
         
-        % decorrelation length scale
+        %%%%% decorrelation length scale
         ldc(ii,i) = length_scale(v(xvl:xvr,ymid,:,tind),1,dx)*4;
         
 %         % check pv 
@@ -163,6 +179,7 @@ for ii=1:length(dirs)
             figure(h2) % on u plot
             linex(edgeloc,''); % mark width
             linex([npvl npvr cpvl cpvr],' ','w');% mark negative + constant pv region
+            pause
         end
     end
 
